f61ec2826c12bb6ee51aa2a73c56d423716c2aa1
MichaelChorak <michael.chorak@hva.nl>
2021-02-09 13:16:00 +0100

    nieuw epublic folder toegevoegd en express static usage

--- back-end/index.js
+++ back-end/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
+app.use(express.static('public'));
+
+
 app.get('/', (req, res) => {
   res.send('Hello big world!')
 });
