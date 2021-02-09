99ba14f5190f483a83c5e934d107693876d61c65
MichaelChorak <michael.chorak@hva.nl>
2021-02-09 12:40:35 +0100

    mappenstrucuur aangepast en overbodige files verwijderd

--- back-end/node/calculate.js
+++
@@ -1,7 +0,0 @@
-// calculate.js
-var math = require ('./lib/math.js');  //.js is optional
-
-console.log(math);
-{
-  add: [function]
-}
--- back-end/node/lib/math.js
+++
@@ -1,5 +0,0 @@
-// lib/math.js
-// add 'add' function to 'exports'
-exports.add = function ( num1, num2 ) {
-  return num1 + num2
-};
