ffe50e2e6a7b8391b40823f9b4faa4ee8b7b762b
MichaelChorak <michael.chorak@hva.nl>
2021-02-11 13:49:03 +0100

    nieuwe map structuur

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('public/static'));
 
 
 app.get('/', (req, res) => {
