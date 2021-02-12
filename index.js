44d7291df83ae763ff5df844439284f02d1a9225
MichaelChorak <michael.chorak@hva.nl>
2021-02-12 16:23:28 +0100

    handlebars toegevoegd, test pagina en juiste routing

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000;
 
-app.use(express.static('public/static'));
+
+const movies = [
+  {title: "333", genre: "333", jaartal: "3333",
+  title: "3433", genre: "334443", jaartal: "3333433",
+  title: "3vfff33", genre: "3v b33", jaartal: "3rfv333"
+}
+]
+
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/movies', (req, res) => {
+  res.render('listOfMovies', {title: "List of Movies", movies})
+});
 
 
 app.get('/', (req, res) => {
-  res.send('Hello big world!')
+  res.render('home', {})
 });
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
+## Project Tech
 
+Hello there - Welcome to my project tech _README_!
+
+Matching application for **Board Games**.
