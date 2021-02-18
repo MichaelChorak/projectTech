249f8ac8de9b7fa511a10512c3392da1291dc760
MichaelChorak <michael.chorak@hva.nl>
2021-02-18 15:23:31 +0100

    Add board game testing with POST/GET

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,20 +1,2 @@
 var hamburger = document.querySelector('button');
 var hamburgerMenu = document.querySelector('#menu');
-
-function menu(){
-  console.log('testjoh');
-
-  hamburgerMenu.animate([
-    //keyframes
-    {transform: 'translateX(20%)'}
-    ], {
-    //timing options
-    duration: 1000,
-    fill: "forwards",
-    easing: "ease-in"
-  });
-
-  
-}
-
-hamburger.addEventListener('click', menu);
