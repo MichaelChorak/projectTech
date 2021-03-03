ea0a3481ef410e97d62ef715ea380618a70207c9
MichaelChorak <michael.chorak@hva.nl>
2021-03-04 00:33:56 +0100

    Beginning of the POST function added

--- index.js
+++ index.js
@@ -4,20 +4,23 @@ const dotenv = require('dotenv').config();
 const config = require('./process.env');
 const express = require('express');
 const exphbs = require('express-handlebars');
+var url = "mongodb://localhost:27017/";
+var mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
-
 const uri = "mongodb+srv://maikiew:Chorak12l3c1%21@cluster0.fiihw.mongodb.net/test?authSource=admin&replicaSet=atlas-r4sakp-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
 const db = new MongoClient(uri, {useUnifiedTopology: true});
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.json());
 db.connect();
 async function run() {
   try {
     // Connect the client to the server
     await db.connect();
     // Establish and verify connection
-    await db.db("admin").command({ ping: 1 });
+    await db.db("maikiew").command({ ping: 1 });
     console.log("Connected successfully to server");
   } finally {
     // Ensures that the client will close when you finish/error
@@ -29,7 +32,6 @@ run().catch(console.dir);
 
 
 
-app.use(bodyParser.urlencoded({ extended: true}));
 
 
 //standaard layout is nu main.hbs en extensie naam is veranderd naar .hbs
@@ -49,6 +51,25 @@ app.get('/movies', (req, res) => {
 app.get('/', (req, res) => {
   res.render('home');
 });
+
+app.post("/addname", (req, res) => {
+  MongoClient.connect(uri, function(err, db) {
+          if (err) throw err;
+          var dbo = db.db("users");
+          dbo.collection("customers").insertOne({
+              naam: req.body.naam,
+              leeftijd: req.body.leeftijd,
+              hallo: req.body.hallo
+          },
+          function(err, result) {
+              if (err) throw err;
+              res.json(result);
+              db.close();
+          });
+      });
+});
+
+
 
 //moviepagina render
 // app.get('/movies', (req, res) => {
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,2 +1,4 @@
 var hamburger = document.querySelector('button');
 var hamburgerMenu = document.querySelector('#menu');
+var volgende = document.querySelector('#volgende');
+var form = document.querySelectorAll("input");
