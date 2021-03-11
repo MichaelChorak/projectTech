029d6b40237726e875144732bf30d44ed728acac
MichaelChorak <michael.chorak@hva.nl>
2021-03-11 12:24:36 +0100

    Changed ports

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(express.static('public'));
 
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
 
 
